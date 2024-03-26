42f40d0694cef3aa3dbfe7b26229c8ae5edfb095
USER <a90521@daum.net>
2024-03-26 12:11:49 +0900

    test

--- 20240326/src/Test005.java
+++ 20240326/src/Test005.java
@@ -1,15 +1,14 @@
 
 public class Test005 {
 
-	public static void main(String[] args, String $d %) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String  _name  = "Hawie";
 		String  $numb = "010-8700-0000";
 		int age = 34;
 		
-		System.out.printf(s %+"이름은 :" + _name);
-		System.out.printf(d % +"나이는 :"  + age);
-		System.out.printf("전화번호는: "+ $numb);
+		System.out.printf("나의 이름은  %s 이고 나이는 %d 이며 전번은 %d", _name,age,$numb);
+	
 	}
 
 }
---
+++ 20240326/src/Test006.java
@@ -0,0 +1,18 @@
+
+public class Test006 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a , b;
+		a = 10;
+		b =5;
+		
+		int c,d;
+		c = a +b;
+		d = a -b;
+		System.out.printf("%d+%d 합은  %d 이고 , %d -%d 는 %d\n" ,a,b,c, a,b ,d);
+		System.out.println("a + b =" + c +"이고 , a -b 는 "+d +"이다");
+		System.out.format("%d+%d 합은  %d 이고 , %d -%d 는 %d\n" ,a,b,c, a,b ,d);
+	}
+
+}
