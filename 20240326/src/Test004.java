4d2ffdb884d56ff516c2a142ed0fdf16b97cdcac
USER <a90521@daum.net>
2024-03-26 11:55:11 +0900

    test

---
+++ 20240326/src/Test004.java
@@ -0,0 +1,14 @@
+
+public class Test004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c ;
+		c = b +a	;
+		System.out.println("값:" + c);
+		
+	}
+
+}
---
+++ 20240326/src/Test005.java
@@ -0,0 +1,15 @@
+
+public class Test005 {
+
+	public static void main(String[] args, String $d %) {
+		// TODO Auto-generated method stub
+		String  _name  = "Hawie";
+		String  $numb = "010-8700-0000";
+		int age = 34;
+		
+		System.out.printf(s %+"이름은 :" + _name);
+		System.out.printf(d % +"나이는 :"  + age);
+		System.out.printf("전화번호는: "+ $numb);
+	}
+
+}
